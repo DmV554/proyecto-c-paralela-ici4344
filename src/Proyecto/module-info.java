d6ed247db854e6a2dcf8d5c28439baef4f9ea045
DmV554 <daniel3318bl@gmail.com>
2025-05-10 19:55:59 -0400

    Refactorización de proyecto, carpeta lib para .jar y module info actualizado

---
+++ src/Proyecto/module-info.java
@@ -0,0 +1,14 @@
+// module-info.java
+// Asegúrate que esté en la raíz de tu código fuente del módulo,
+// por ejemplo: TareaProject/src/Tarea/module-info.java
+
+module Proyecto { // Este es el nombre de TU módulo
+
+	exports common;
+	requires java.rmi;
+	requires com.fasterxml.jackson.databind;
+	requires com.fasterxml.jackson.core;
+	requires com.fasterxml.jackson.annotation;
+	requires org.apache.httpcomponents.client5.httpclient5;
+	requires org.apache.httpcomponents.core5.httpcore5;
+}
