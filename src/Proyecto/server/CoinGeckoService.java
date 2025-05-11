d80a17e66baa4c42b37f1688ce08e98dc97511bc
DmV554 <daniel3318bl@gmail.com>
2025-05-10 23:15:55 -0400

    Integración conexión con API para obtener precios de criptomonedas. Sin DB

--- src/Proyecto/server/CoinGeckoService.java
+++ src/Proyecto/server/CoinGeckoService.java
@@ -1,22 +1,4 @@
-package server; // O server.external
-/*
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.ParseException;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
+package server;
 
-import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-public class CoinGeckoService {
-
-}*/
+public class GeckoClient {
+}
